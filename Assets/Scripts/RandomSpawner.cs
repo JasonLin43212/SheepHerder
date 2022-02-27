bf8d64167bf9fd80fef937edf4fff163819fce16
Peiqi Chen <peiqic@mit.edu>
2022-02-27 16:49:35 -0500

    Added a limit of sheep spawned to the spawning

--- Assets/Scripts/RandomSpawner.cs
+++ Assets/Scripts/RandomSpawner.cs
@@ -6,6 +6,8 @@ public class RandomSpawner : MonoBehaviour
 {
     [SerializeField] private GameObject ItemPrefab; //public GameObject ItemPrefab;
     [SerializeField] private float radius = 1; //public float radius = 1;
+    [SerializeField] private float totalSheep = 3; 
+    private float counter = 0; 
     // Start is called before the first frame update
     void Start()
     {
@@ -14,8 +16,11 @@ public class RandomSpawner : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        Invoke("SpawnObjectAtRandom", 3.0F); 
+    {   
+        if (counter < totalSheep){
+            Invoke("SpawnObjectAtRandom", 3.0F); 
+        }
+        //Invoke("SpawnObjectAtRandom", 3.0F); 
         // if (Input.GetKeyDown(KeyCode.Space)) {
         //     SpawnObjectAtRandom();
         // }
@@ -26,6 +31,7 @@ public class RandomSpawner : MonoBehaviour
         Vector3 randomPos = Random.insideUnitCircle * radius;
         Instantiate(ItemPrefab, this.transform.position + randomPos, Quaternion.identity); //Instantiate(ItemPrefab, this.transform.position + randomPos, Quaternion.identity);
         CancelInvoke();
+        counter = counter + 1;
     }
 
     private void OnDrawGizmos() {
