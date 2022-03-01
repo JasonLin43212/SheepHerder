c56423c1b0ea6e4e1b59f61499839b1e7b0badb3
Jason Lin <jasonlin43212@gmail.com>
2022-03-01 16:14:15 -0500

    Have some version of dashing

--- Assets/Scripts/Dog.cs
+++ Assets/Scripts/Dog.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class Dog : MonoBehaviour
 {
-    [SerializeField] private float speed;
     [SerializeField] private System.String playerNumber;
     private Rigidbody2D body;
+    private Vector2 prevVelocity;
+    private float dashSpeed = 20;
+    private float speed = 4;
+    private bool isDashing;
+    private int dashingCooldown;
 
     // Start is called before the first frame update
     void Start()
@@ -17,9 +21,24 @@ public class Dog : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal" + playerNumber);
-        float verticalInput = Input.GetAxis("Vertical" + playerNumber);
-
-        body.velocity = new Vector2(horizontalInput * speed, verticalInput * speed);
+        KeyCode dashKeyCode = playerNumber == "1"
+            ? KeyCode.BackQuote
+            : KeyCode.Comma;
+        
+        if (isDashing) {
+            body.velocity = prevVelocity * dashSpeed;
+            dashingCooldown--;
+            if (dashingCooldown <= 0) {
+                isDashing = false;
+            }
+        } else if (Input.GetKeyDown(dashKeyCode)) {
+            isDashing = true;
+            dashingCooldown = 15;
+        } else {
+            float horizontalInput = Input.GetAxis("Horizontal" + playerNumber);
+            float verticalInput = Input.GetAxis("Vertical" + playerNumber);
+            body.velocity = new Vector2(horizontalInput * speed, verticalInput * speed);
+            prevVelocity = body.velocity.normalized;
+        }
     }
 }
