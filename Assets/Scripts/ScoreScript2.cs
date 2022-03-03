daab74a0203d7a11c0d0850cf23f57261f14405e
Meenakshi Singh <mrsingh0306@gmail.com>
2022-03-03 00:06:26 -0500

    scoring system for two players

--- Assets/Scripts/ScoreScript2.cs
+++ Assets/Scripts/ScoreScript2.cs
@@ -3,16 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ScoreScript : MonoBehaviour
+public class ScoreScript2 : MonoBehaviour
 {
-    public static ScoreScript instance;
+    public static ScoreScript2 instance;
 
-    public int player1Score = 0;
-/*    public int player2Score = 0;
-*/
+    public int player2Score = 0;
+
     public Text score;
-/*    public Text score2;
-*/
+
     private void Awake()
     {
         instance = this;
@@ -21,18 +19,16 @@ public class ScoreScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        score.text = "Player 1 Score: " + player1Score.ToString();
+        score.text = "Player 2 Score: " + player2Score.ToString();
         score = GetComponent<Text>();
 
-/*        score2.text = "Player 2 Score: " + player2Score.ToString();
-        score2 = GetComponent<Text>();*/
     }
 
     void Update()
     {
-        player1Score = 0;
-/*        player2Score = 0;
-*/        GameObject[] blackSheepSpawned;
+        player2Score = 0;
+
+        GameObject[] blackSheepSpawned;
         blackSheepSpawned = GameObject.FindGameObjectsWithTag("BlackSquare");
 
         GameObject[] whiteSheepSpawned;
@@ -43,19 +39,11 @@ public class ScoreScript : MonoBehaviour
             float sheepX = sheep.transform.position.x;
             float sheepY = sheep.transform.position.y;
 
-            //upper left square
-            if (sheepX > -16 && sheepX < -10 && sheepY < 9 && sheepY > 3)
-            {
-                player1Score += 1;
-            }
-
-
-/*
             //lower right square
             if (sheepX > 9 && sheepX < 15 && sheepY < -3 && sheepY > -9)
             {
                 player2Score += 1;
-            }*/
+            }
 
 
         }
@@ -66,21 +54,15 @@ public class ScoreScript : MonoBehaviour
             float sheepX = sheep.transform.position.x;
             float sheepY = sheep.transform.position.y;
 
- /*           //upper right square
+            //upper right square
             if (sheepX > 9 && sheepX < 15 && sheepY < 9 && sheepY > 3)
             {
                 player2Score += 1;
-            }*/
-
-            //lower left square
-            if (sheepX > -16 && sheepX < -10 && sheepY < -3 && sheepY > -9)
-            {
-                player1Score += 1;
             }
 
         }
 
 
-        score.text = "Player 1 Score: " + player1Score.ToString();
+        score.text = "Player 2 Score: " + player2Score.ToString();
     }
 }
